78b347d41405590f944b9b8cbd8d889ba57047ba
vu nguyen <vutrinhsat2023@gmail.com>
2023-11-29 10:14:39 +0700

    first commit

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+.header_title {
+    color:red;
+    background-color: aqua;
+    text-align: center;
+}
+</style>
+</head>
+<body>
+<h2 class="header_title">Danh mục sản phẩm</h2>
+        <table>
+        <tr>
+            <th>STT</th>
+            <th>Tên danh mục</th>
+            <th>Trạng thái</th>
+            <th>Hành động</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        </table>
+
+</body>
+</html>
+
